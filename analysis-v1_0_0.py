6c492c8a14a575ae5fc7325afb69764f15124ebc
ForeSight-Research-Unlimited <132500006+ForeSight-Research-Unlimited@users.noreply.github.com>
2023-08-22 23:03:04 +0530

    22082023

--- analysis-v1_0_0.py
+++ analysis-v1_0_0.py
@@ -12,10 +12,18 @@ intv = '15m'
 
 yfTicker = yf.Ticker(ticker1)
 
-
 data1 = pd.DataFrame(yfTicker.history(interval= intv))
+#print(data1.head())
 
-data1.to_csv("ForeSight-Research-Unlimited/ema_convergence_strat/data/%s-%s.csv" %(ticker1, intv))
+#data1.to_csv("/data/%s-%s.csv" %(ticker1, intv))
 
+data1['EMA10'] = data1["Close"].ewm( span = 10 ).mean()
+data1['EMA25'] = data1["Close"].ewm( span = 25 ).mean()
+data1['EMA50'] = data1["Close"].ewm( span = 50 ).mean()
+data1['EMA100'] = data1["Close"].ewm( span = 100 ).mean()
+data1['EMA200'] = data1["Close"].ewm( span = 200 ).mean()
+data1['EMA500'] = data1["Close"].ewm( span = 500 ).mean()
+
+print(data1.tail())
 
 code.interact(local=locals())
